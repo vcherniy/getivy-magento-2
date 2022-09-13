486edeaffc37474a83d1ee472551cec937f5a6be
Muhib <muhib.khan@esparksinc.com>
2022-09-13 16:51:01 +0500

    Fix default store logo issue

--- Observer/UpdateMerchant.php
+++ Observer/UpdateMerchant.php
@@ -6,6 +6,8 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Config\Storage\WriterInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\UrlInterface;
 use GuzzleHttp\Client;
 
 class UpdateMerchant implements ObserverInterface
@@ -17,6 +19,8 @@ class UpdateMerchant implements ObserverInterface
     protected $storeManager;
     protected $logo;
     protected $emulation;
+    protected $scopeConfig;
+    protected $urlBuilder;
 
     public function __construct(
         RequestInterface $request, 
@@ -25,7 +29,9 @@ class UpdateMerchant implements ObserverInterface
         \Magento\Framework\Serialize\Serializer\Json $json,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Theme\Block\Html\Header\Logo $logo,
-        \Magento\Store\Model\App\Emulation $emulation
+        \Magento\Store\Model\App\Emulation $emulation,
+        ScopeConfigInterface $scopeConfig,
+        UrlInterface $urlBuilder
     )
     {
         $this->request = $request;
@@ -35,6 +41,8 @@ class UpdateMerchant implements ObserverInterface
         $this->logo = $logo;
         $this->storeManager = $storeManager;
         $this->emulation = $emulation;
+        $this->scopeConfig = $scopeConfig;
+        $this->urlBuilder = $urlBuilder;
     }
     public function execute(EventObserver $observer)
     {
@@ -45,7 +53,18 @@ class UpdateMerchant implements ObserverInterface
         $webhookUrl = $frontendUrl.'ivypayment/webhook';
         $completeCallbackUrl = $frontendUrl.'ivypayment/order/complete';
         $this->emulation->startEnvironmentEmulation(null, \Magento\Framework\App\Area::AREA_FRONTEND, true);
-        $shopLogo = $this->logo->getLogoSrc();
+        $path = $this->scopeConfig->getValue(
+            'design/header/logo_src',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        if($path)
+        {
+            $shopLogo = $this->urlBuilder
+                ->getBaseUrl(['_type' => \Magento\Framework\UrlInterface::URL_TYPE_MEDIA]) .'logo/'. $path;
+        }
+        else{
+            $shopLogo = $this->logo->getLogoSrc();
+        }
         $this->emulation->stopEnvironmentEmulation();
 
         // Create data to set in api
